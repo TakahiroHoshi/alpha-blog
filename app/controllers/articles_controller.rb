b1dd9cacbc3427910e720c9d21cd86a99d182cf8
stars <loinzaft@gmail.com>
2016-06-21 00:54:20 +0900

    extracted redundancies

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,15 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
 
-  def new
-
-    @article = Article.new
-
+  def index
+    @articles = Article.all
   end
 
+  def new
+    @article = Article.new
+  end
 
   def create
-
     @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was successfully created"
@@ -18,20 +19,38 @@ class ArticlesController < ApplicationController
     end
   end
 
-
   def show
 
-    @article = Article.find(params[:id])
+  end
 
+  def edit
+
+  end
+
+  def update
+    if @article.update(article_params)
+      flash[:notice] = 'Aritcle was successfully updated'
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article.destroy
+    flash[:notice] = 'Article was successfully deleted'
+    redirect_to articles_path
   end
 
 
   private
 
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
     def article_params
-
       params.require(:article).permit(:title, :description)
-
     end
 
 end
