23c9b13f0ebcfd8e6f2037a2dd681d9f82969f76
= <loinzaft@gmail.com>
2016-07-10 17:21:31 +0900

    up to session 116

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,10 @@ Rails.application.routes.draw do
   resources :articles
   resources :users
 
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
