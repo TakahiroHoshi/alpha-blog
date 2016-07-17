eaa5637220fc00e4f69b32ceba19ede869530f55
= <loinzaft@gmail.com>
2016-07-17 12:22:57 +0900

    done adding categories controller

--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,12 @@ Rails.application.routes.draw do
 
   resources :articles
   resources :users
+  resources :categories
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
