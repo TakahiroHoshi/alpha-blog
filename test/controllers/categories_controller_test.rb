278cd698b996897d20dcae88e5a1867eccdd21d6
= <loinzaft@gmail.com>
2016-07-18 13:19:25 +0900

    categories test done

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -4,6 +4,7 @@ class CategoriesControllerTest < ActionController::TestCase
 
   def setup
     @category = Category.create(name: "sports")
+    @user = User.create(name: "admin", email: "admin@aef.aef", password: "password", admin: true)
   end
 
   test "should get categories index" do
@@ -12,6 +13,7 @@ class CategoriesControllerTest < ActionController::TestCase
   end
 
   test "should get categories new" do
+    session[:user_id] = @user.id
     get :new
     assert_response :success
   end
@@ -21,5 +23,11 @@ class CategoriesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-
+  test "should redirect create when admin not logged in" do
+    assert_no_difference 'Category.count' do
+      post :create, category: {name: "sports"}
+    end
+    assert_redirected_to categories_path
+    #assert_match "Only admin can perform that action"
+  end
 end
